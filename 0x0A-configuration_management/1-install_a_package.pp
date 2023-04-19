4c5429964ca996e81e20c9880aa965935466bb6d
akubosylva <akubosylva@gmail.com>
2023-04-19 01:55:52 +0100

    Modify Files

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,6 +0,0 @@
-# Using Puppet, install flask
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
-}
--- attack_is_the_best_defense/README.md
+++
@@ -1,4 +0,0 @@
-Attack is the best defense
-
-An optional task
-I need more marks to make it in this sprint
